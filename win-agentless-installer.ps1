90b55ed97ad10b98f9fbd33ad19a3dbf5a00d6f1
Bartosz Lukowicz <bartosz.lukowicz@eyer.ai>
2024-05-28 13:52:16 +0200

    installer PoC

---
+++ win-agentless-installer.ps1
@@ -0,0 +1,185 @@
+function PrepareFolder {
+    param (
+        [string]$folderPath
+    )
+
+    if (!(Test-Path $folderPath)) {
+        Write-Host "[>_] Creating folder: $folderPath"
+        New-Item -ItemType Directory -Path $folderPath | Out-Null
+    }
+}
+
+function Download {
+    param (
+        [string]$url,
+        [string]$destinationPath
+    )
+
+    if (!(Test-Path $destinationPath)) {
+        Write-Host "[>_] Downloading $url to $destinationPath..."
+        wget $url -UseBasicParsing -OutFile $destinationPath
+    }
+}
+
+function SetupJetty {
+    param (
+        [string]$jettyHome,
+        [string]$jettyBase
+    )
+
+    Write-Host "[>_] Setting up Jetty..."
+
+    $jettyZipPath = "$env:APPDATA\eyer\downloads\jetty-home-12.0.9.zip"
+    $jettyExtractPath = "$env:APPDATA\eyer\downloads\jetty-home-12.0.9"
+
+    # Download and extract Jetty
+    Write-Host "[>_] Downloading and extracting Jetty..."
+    Download "https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-home/12.0.9/jetty-home-12.0.9.zip" $jettyZipPath
+    Expand-Archive -Force -Path $jettyZipPath -DestinationPath $jettyExtractPath
+
+    # Copy Jetty files to the specified locations
+    Write-Host "[>_] Copying Jetty files to $jettyHome..."
+    Copy-Item -Path "$jettyExtractPath\*" -Destination $jettyHome -Recurse -Force
+}
+
+function SetupTelegraf {
+    param (
+        [string]$eyerTelegrafPath
+    )
+
+    Write-Host "[>_] Setting up Telegraf..."
+
+    $telegrafPath = "C:\Program Files\InfluxData"
+
+    $telegrafZipPath = "$env:APPDATA\eyer\downloads\telegraf-1.30.3_windows_amd64.zip"
+    $telegrafExtractPath = "$env:APPDATA\eyer\downloads\telegraf-1.30.3"
+
+    Write-Host "[>_] Downloading and extracting Telegraf..."
+    Download "https://dl.influxdata.com/telegraf/releases/telegraf-1.30.3_windows_amd64.zip" $telegrafZipPath
+    Expand-Archive -Force -Path $telegrafZipPath -DestinationPath $telegrafExtractPath
+
+    Copy-Item -Path "$telegrafExtractPath\*" -Destination $eyerTelegrafPath -Recurse -Force
+
+    # Create a symbolic link to the Telegraf installation
+    if (!(Test-Path "$telegrafPath\telegraf")) {
+        Write-Host "[>_] Creating symbolic link to Telegraf installation..."
+        PrepareFolder $telegrafPath
+        New-Item -ItemType SymbolicLink -Path "$telegrafPath\telegraf" -Target $eyerTelegrafPath # | Out-Null
+    } else {
+        Write-Host "[>_] Symbolic link already exists."
+    }
+}
+
+function Test-Jetty {
+    param ()
+
+    $process = Start-Process powershell -ArgumentList '-NoExit', '-Command', 'java -jar $env:JETTY_HOME\start.jar' -PassThru
+
+    # Define the end time for the test (15 seconds from now)
+    $endTime = (Get-Date).AddSeconds(30)
+    $url = "http://localhost:8080/jolokia"
+    # Variable to track success
+    
+    Write-Host "[>_] Testing Jetty" -NoNewline
+    
+    $success = $false
+    # Test the URL every second for the specified duration
+    while ((Get-Date) -lt $endTime) {
+        try {
+            $webRequest = Invoke-WebRequest -Uri $url -UseBasicParsing -TimeoutSec 1
+            if ($webRequest.StatusCode -eq 200) {
+                $success = $true
+                break
+            }
+        } catch {
+            $success = $false
+        }
+        Write-Host "."  -NoNewline
+        Start-Sleep -Seconds 1
+    }
+    
+    # Stop the process
+    Get-CimInstance Win32_Process | Where-Object { $_.ParentProcessId -eq $process.Id } | ForEach-Object { Stop-Process -Id $_.ProcessId }
+    Stop-Process -Id $process.Id
+    
+    # Check success and report
+    if ($success) {
+        Write-Host " success"
+    } else {
+        Write-Host " fail"
+    }
+}
+
+function Test-Telegraf {
+    param ()
+
+    $telegrafPath = "C:\Program Files\InfluxData\telegraf\telegraf.exe"
+    $configPath = "C:\Program Files\InfluxData\telegraf\telegraf.conf"
+    $command = "--config '$configPath' --test"
+
+    Write-Host "[>_] Testing Telegraf..." -NoNewline
+    $process = Start-Process -FilePath "$telegrafPath" -ArgumentList "$command" -PassThru
+    $process.WaitForExit()
+    
+    $success = $false
+    if ($process.ExitCode -eq 0) {
+        # Collect output from the console (stdout)
+        $output = & $command
+
+        # Check if the output contains metrics
+        if ($output) {
+            $success = $true
+        }
+    }
+
+    # Check success and report
+    if ($success) {
+        Write-Host " success"
+    } else {
+        Write-Host " fail"
+    }
+}
+
+# Check if Java is installed
+$javaVersion = (Get-Command java).Version
+if ($javaVersion -eq $null) {
+    Write-Host "Java is required to run this script."
+    exit
+}
+
+Write-Host "[>_] Java version found: $javaVersion"
+
+# Prepare required folders
+Write-Host "[>_] Preparing required folders..."
+PrepareFolder "$env:APPDATA\eyer"
+PrepareFolder "$env:APPDATA\eyer\downloads"
+PrepareFolder "$env:APPDATA\eyer\telegraf"
+PrepareFolder "$env:APPDATA\eyer\jetty\home"
+PrepareFolder "$env:APPDATA\eyer\jetty\base"
+
+# Download and setup Jetty
+SetupJetty "$env:APPDATA\eyer\jetty\home" "$env:APPDATA\eyer\jetty\base"
+
+# Download and setup Telegraf
+SetupTelegraf "$env:APPDATA\eyer\telegraf"
+
+# Start Jetty
+Write-Host "[>_] Starting Jetty..."
+$env:JETTY_HOME = "$env:APPDATA\eyer\jetty\home"
+$env:JETTY_BASE = "$env:APPDATA\eyer\jetty\base"
+
+cd $env:JETTY_BASE
+
+java -jar $env:JETTY_HOME\start.jar --add-modules=server,http,ee10-deploy
+java -jar $env:JETTY_HOME\start.jar --add-module=demos
+cp "$env:APPDATA\eyer\downloads\jolokia-agent-war-unsecured-2.0.2.war" "$env:JETTY_BASE\webapps"
+mv "$env:JETTY_BASE\webapps\jolokia-agent-war-unsecured-2.0.2.war" "$env:JETTY_BASE\webapps\jolokia.war" 
+
+Test-Jetty
+
+# Start Telegraf
+$telegrafExePath = "$env:APPDATA\eyer\telegraf\telegraf.exe"
+Write-Host "[>_] Installing Telegraf..."
+Start-Process -FilePath $telegrafExePath -ArgumentList "--service", "install", "--config", "C:\Program Files\InfluxData\telegraf\telegraf.conf"
+
+Write-Host "[>_] Installation completed"
