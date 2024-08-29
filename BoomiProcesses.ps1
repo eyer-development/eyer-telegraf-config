bba2338aa8641729c0d8f6f7740578c15e923300
Marius <148550505+mwstorsten@users.noreply.github.com>
2024-08-29 09:52:49 +0200

    Update BoomiProcesses.ps1

--- BoomiProcesses.ps1
+++ BoomiProcesses.ps1
@@ -6,7 +6,9 @@ $boomiAccountId = "your_Boomi_account_Id"
 
 #--------------------------------------------------------------------------------
 
-$Remove = $TelegrafPath + 'Transactions*.json'
+function RegularProcess {
+
+$Remove = $TelegrafPath + 'regular*.json'
 Remove-Item $Remove
 $Time = Get-Date
 $UTC = $Time.ToUniversalTime()
@@ -39,7 +41,7 @@ $Response = Invoke-RestMethod -Headers $headers -Method $Method -URI $URI -Body 
 $json = $Response | ConvertTo-Json -Depth 2
 $json = $json.Replace("Long ","")
 $json = $json -replace '"(\d+)"', '$1'
-$WriteResponseInitial = $TelegrafPath + 'Transactions0.json'
+$WriteResponseInitial = $TelegrafPath + 'regular0.json'
 $json | Out-File -FilePath $WriteResponseInitial
 
 #Check for more transactions from Boomi
@@ -57,8 +59,74 @@ $Response = Invoke-RestMethod -Headers $headers -Method $Method -URI $URI -Body 
 $json = $Response | ConvertTo-Json -Depth 2
 $json = $json.Replace("Long ","")
 $json = $json -replace '"(\d+)"', '$1' 
-$WriteResponse = $TelegrafPath + 'Transactions' + $Number
+$WriteResponse = $TelegrafPath + 'regular' + $Number
 $json | Out-File -FilePath ($WriteResponse + '.json')
 $Number++
  }   
 }
+}
+
+#--------------------------------------------------------------------------------
+
+function LowLatencyProcess {
+
+$Remove = $TelegrafPath + 'lowLatency*.json'
+Remove-Item $Remove
+$Time = Get-Date
+$UTC = $Time.ToUniversalTime()
+$UTCOffset = $UTC.AddMinutes(-6)
+$UTC_Final1 = $UTCOffset.ToString("yyyy-MM-ddTHH:mm:00Z")
+$UTC_Final2 = $UTCOffset.ToString("yyyy-MM-ddTHH:mm:59Z")
+
+$Method = "POST"
+$URI = "https://api.boomi.com/api/rest/v1/" + $boomiAccountId + "/ExecutionSummaryRecord/query"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $boomi_username,$boomi_token)))
+$headers = @{
+     Authorization=("Basic {0}" -f $base64AuthInfo)
+    'Accept' = 'application/json'
+    'Content-Type' = 'application/json'
+}
+$Body = @"
+{
+    "QueryFilter":
+    {
+    "expression":
+    {
+    "argument": ["$UTC_Final1","$UTC_Final2"],
+    "operator": "BETWEEN",
+    "property": "timeBlock"
+        }
+    }
+}
+"@
+#Fetch initial 100 transactions
+$Response = Invoke-RestMethod -Headers $headers -Method $Method -URI $URI -Body $Body
+$json = $Response | ConvertTo-Json -Depth 2
+#$json = $json.Replace("Long ","")
+#$json = $json -replace '"(\d+)"', '$1'
+$WriteResponseInitial = $TelegrafPath + 'lowLatency0.json'
+$json | Out-File -FilePath $WriteResponseInitial
+
+#Check for more transactions from Boomi
+if ($Response.queryToken.Length -gt 0) {
+$URI = "https://api.boomi.com/api/rest/v1/" + $BoomiAccountId + "/ExecutionSummaryRecord/queryMore"
+$Number=1
+
+while ($Response.queryToken.Length -gt 0) {
+$Token = $Response.queryToken | ConvertTo-Json
+$Body = @"
+    $Token
+"@
+
+$Response = Invoke-RestMethod -Headers $headers -Method $Method -URI $URI -Body $Body
+$json = $Response | ConvertTo-Json -Depth 2
+$json = $json.Replace("Long ","")
+$json = $json -replace '"(\d+)"', '$1' 
+$WriteResponse = $TelegrafPath + 'lowLatency' + $Number
+$json | Out-File -FilePath ($WriteResponse + '.json')
+$Number++
+ }   
+}
+}
+ 
+
