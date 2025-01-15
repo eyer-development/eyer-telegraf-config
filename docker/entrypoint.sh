a6d8517e2b61242ac28dc6bdb05adda5a18c88e8
Bartosz Lukowicz <bartosz.lukowicz@eyer.ai>
2025-01-15 16:31:38 +0100

    run base telegraf only if BOOMI_ATOM_IP is present

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-if [ -f "$TELEGRAF_HOME/telegraf.conf" ]; then
-    echo "[>_] Found Telegraf configuration: telegraf.conf"
-else
-    echo "[>_] Telegraf configuration is missing."
-    echo "[!] Please ensure that the Telegraf configuration file is passed to the container."
-    echo "[!] Use a volume mount or another method to provide the file at the path: $TELEGRAF_HOME/telegraf.conf"
-    exit 1
-fi
-
 # Check for Boomi and Eyer environment variables
-if [ -z "$BOOMI_USERNAME" ] || [ -z "$BOOMI_TOKEN" ] || [ -z "$BOOMI_ACCOUNT_ID" ] || [ -z "$EYER_PUSH_TOKEN" ] || [ -z "$BOOMI_ATOM_IP" ]; then
+if [ -z "$BOOMI_USERNAME" ] || [ -z "$BOOMI_TOKEN" ] || [ -z "$BOOMI_ACCOUNT_ID" ] || [ -z "$EYER_PUSH_TOKEN" ]; then
     echo "[!] One or more required environment variables are missing:"
     [ -z "$BOOMI_USERNAME" ] && echo "    - BOOMI_USERNAME is not set."
     [ -z "$BOOMI_TOKEN" ] && echo "    - BOOMI_TOKEN is not set."
     [ -z "$BOOMI_ACCOUNT_ID" ] && echo "    - BOOMI_ACCOUNT_ID is not set."
     [ -z "$EYER_PUSH_TOKEN" ] && echo "    - EYER_PUSH_TOKEN is not set."
-    [ -z "$BOOMI_ATOM_IP" ] && echo "    - BOOMI_ATOM_IP is not set."
     exit 1
 fi
 
@@ -24,11 +14,16 @@ fi
 echo "[>_] Using Boomi and Eyer credentials:"
 echo "    Boomi Username: $BOOMI_USERNAME"
 echo "    Boomi Account ID: $BOOMI_ACCOUNT_ID"
-echo "    Boomi Atom IP: $BOOMI_ATOM_IP"
+echo "    Boomi Atom IP: ${BOOMI_ATOM_IP:-(empty)}"
 
-# Start Jetty
-echo "[>_] Starting Services..."
-supervisord -c /opt/supervisord.conf
+if [ -z "$BOOMI_ATOM_IP" ]; then
+    echo "[!] BOOMI_ATOM_IP is not set. Starting monitoring-only services."
+    supervisord_config="/opt/supervisord_monitoring_only.conf"
+else
+    echo "[>_] BOOMI_ATOM_IP is set. Starting full services."
+    supervisord_config="/opt/supervisord.conf"
+fi
+supervisord -c "$supervisord_config"
 
 # Keep container running
 tail -f /dev/null
